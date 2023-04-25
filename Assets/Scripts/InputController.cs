823d1ff674b4245b70bd96db347a5019ad680e6a
Manjit Bedi <dev@manjitbedi.com>
2023-04-25 11:54:12 -0700

    added some movement

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Interactions;
 
 using TMPro;
+using Unity.VisualScripting;
 
 public class InputController : MonoBehaviour
 {
@@ -14,7 +15,17 @@ public class InputController : MonoBehaviour
     [SerializeField]
     TMP_Text movementText;
 
+    [SerializeField]
+    GameObject playerGameObject;
+
+    public float moveSpeed;
+
+    /// <summary>
+    /// value read in from game pad
+    /// </summary>
     Vector2 moveAmount;
+
+    private bool isMovingHorizontally = true;
 
     void Awake()
     {
@@ -34,13 +45,45 @@ public class InputController : MonoBehaviour
         
     }
 
+    #region Game Update
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMovingHorizontally)
+        {
+            MoveHorizontally(moveAmount);
+        }
+        else
+        {
+            Move(moveAmount);
+        }
     }
 
-    #region
+    private void MoveHorizontally(Vector2 direction)
+    {
+        if (direction.sqrMagnitude < 0.01)
+            return;
+        var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+        var move = Quaternion.Euler(0, playerGameObject.transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, 0);
+        playerGameObject.transform.position += move * scaledMoveSpeed;
+    }
+
+
+    private void Move(Vector2 direction)
+    {
+        if (direction.sqrMagnitude < 0.01)
+            return;
+        var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+        // For simplicity's sake, we just keep movement in a single plane here. Rotate
+        // direction according to world Y rotation of player.
+        var move = Quaternion.Euler(0, playerGameObject.transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
+        playerGameObject.transform.position += move * scaledMoveSpeed;
+    }
+
+    #endregion
+
+    #region Input
 
     public void OnMove(InputAction.CallbackContext context)
     {
